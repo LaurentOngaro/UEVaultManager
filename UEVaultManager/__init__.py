0d60dd5bd8c0474b1b6b583e7dc5958d0f7d6f89
LaurentOngaro <laurent@gameamea.com>
2023-03-29 18:42:11 +0200

    minor release for read the doc integration

--- UEVaultManager/__init__.py
+++ UEVaultManager/__init__.py
@@ -1,11 +1,10 @@
 """UEVaultManager"""
 
 __name__ = 'UEVaultManager'
-__version__ = '1.0.0'
-__codename__ = 'Dragon'
+__version__ = '1.0.1'
+__codename__ = 'Dragon+'
 __author__ = 'Laurent Ongaro'
 __author_email__ = 'laurent@gameamea.com'
-__license__ = 'BSD 3-Clause License'
 __description__ = 'Free and open-source replacement for the Epic Games Launcher application, mainly to manage the assets for Unreal Engine'
 __copyright__ = '2023 Laurent Ongaro'
 __license__ = 'GPL-3'
