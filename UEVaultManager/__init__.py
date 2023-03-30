ea9b465c4b5a7a49fac6c0a130d27dc3f932a4f3
LaurentOngaro <laurent@gameamea.com>
2023-03-30 08:50:23 +0200

    fix missing readme.md for pypi and minor changes

--- UEVaultManager/__init__.py
+++ UEVaultManager/__init__.py
@@ -1,11 +1,15 @@
 """UEVaultManager"""
 
+import datetime
+
+year = datetime.date.today().year
+
 __name__ = 'UEVaultManager'
-__version__ = '1.0.2'
-__codename__ = 'Dragon+2'
+__version__ = '1.0.3'
+__codename__ = 'Dragon+3'
 __author__ = 'Laurent Ongaro'
 __author_email__ = 'laurent@gameamea.com'
 __description__ = 'Free and open-source replacement for the Epic Games Launcher application, mainly to manage the assets for Unreal Engine'
-__copyright__ = '2023 Laurent Ongaro'
+__copyright__ = f'{year} {__author__}'
 __license__ = 'GPL-3'
 __url__ = 'https://github.com/LaurentOngaro/UEVaultManager'
--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ import sys
 
 from setuptools import setup
 
-from UEVaultManager import __name__, __version__, __license__, __author__, __author_email__, __copyright__, __description__
+from UEVaultManager import __name__, __version__, __license__, __author__, __author_email__, __copyright__, __description__,__url__
 
 if sys.version_info < (3, 9):
     sys.exit('python 3.9 or higher is required for UEVaultManager')
 
 root_path = os.path.dirname(os.path.abspath(__file__))
-readme_path = os.path.abspath(os.path.join(root_path, "README.rst"))
+readme_path = os.path.abspath(os.path.join(root_path, 'README.md'))
 
-with open(readme_path, "r") as fh:
+with open(readme_path, 'r') as fh:
     long_description_l = fh.readlines()
-    del long_description_l[4:10]  # keep only description text
-    long_description = ''.join(long_description_l)
+    long_description = ''.join(long_description_l[8:16])  # keep only description text
 
 setup(
     name=__name__,
@@ -27,25 +26,35 @@ setup(
     author_email=__author_email__,
     copyright=__copyright__,
     description=__description__,
+    url=__url__,
     packages=[
-        'UEVaultManager', 'UEVaultManager.api', 'UEVaultManager.downloader', 'UEVaultManager.downloader.mp', 'UEVaultManager.lfs',
-        'UEVaultManager.models', 'UEVaultManager.utils',
+        'UEVaultManager',  #
+        'UEVaultManager.api',  #
+        'UEVaultManager.downloader',  #
+        'UEVaultManager.downloader.mp',  #
+        'UEVaultManager.lfs', 'UEVaultManager.models',  #
+        'UEVaultManager.utils'
     ],
     entry_points=dict(console_scripts=['UEVaultManager = UEVaultManager.cli:main']),
-    install_requires=['requests<3.0', 'setuptools', 'wheel'],
-    extras_require=dict(webview=['pywebview>=3.4'], webview_gtk=['pywebview>=3.4', 'PyGObject']),
-    url='https://github.com/LaurentOngaro/UEVaultManager',
-    long_description_content_type="text/markdown",
+    install_requires=[
+        'requests<3.0',  #
+        'setuptools',  #
+        'wheel'
+    ],
+    extras_require=dict(webview=['pywebview>=3.4'], webview_gtk=[
+        'pywebview>=3.4',  #
+        'PyGObject'
+    ]),
+    long_description_content_type='text/markdown',
     python_requires='>=3.9',
     classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
+        'License :: OSI Approved :: BSD License',  #
+        'Programming Language :: Python :: 3.9',  #
+        'Programming Language :: Python :: 3.10', 'Programming Language :: Python :: 3.11',  #
+        'Operating System :: OS Independent',  #
+        'Development Status :: 4 - Beta'
     ]
 )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     setup()
