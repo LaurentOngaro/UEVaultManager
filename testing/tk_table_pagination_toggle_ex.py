ae5d0eb7ab15e4f23fa01864a739d6e6a037cee1
LaurentOngaro <laurent@gameamea.com>
2023-04-13 19:28:26 +0200

    minor changes

--- testing/tk_table_pagination_toggle_ex.py
+++ testing/tk_table_pagination_toggle_ex.py
@@ -1,38 +1,42 @@
+import os.path
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 from pandastable import Table
+from tkinter import filedialog
+
+file = '../results/list.csv'
 
 
 class EditableTable(tk.Frame):
 
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
-        self.master.geometry('600x400+200+100')
+    def __init__(self, container_frame):
+        tk.Frame.__init__(self, container_frame)
+        self.container_frame = container_frame
+        self.master.geometry('1200x880')
         self.master.title('Table app')
-        self.currentPage = 0
-        self.rowsPerPage = 10
-        self.paginationEnabled = True
+        self.current_page = 0
+        self.rowsPerPage = 35
+        self.pagination_enabled = True
 
         # Load data from CSV file
-        self.df = pd.read_csv('../results/list.csv')
+        self.df = pd.read_csv(file)
 
         # Initialize filtered DataFrame
-        self.df_filtered = self.df
+        self.data_filtered = self.df
 
         # Create frame for table
         f = tk.Frame(self.master)
         f.pack(fill=tk.BOTH, expand=1)
 
         # Create table to display data
-        self.table = Table(f, dataframe=self.df.iloc[0:0], showtoolbar=False, showstatusbar=False)
+        self.table = Table(f, dataframe=self.df.iloc[0:0], showtoolbar=True, showstatusbar=True)
         self.table.show()
 
         # Add buttons for pagination
         self.prevButton = tk.Button(self.master, text='Previous', command=self.prev_page)
         self.prevButton.pack(side=tk.LEFT)
-        tk.Label(self.master, textvariable=self.currentPage).pack(side=tk.LEFT)
+        tk.Label(self.master, textvariable=self.current_page).pack(side=tk.LEFT)
         self.nextButton = tk.Button(self.master, text='Next', command=self.next_page)
         self.nextButton.pack(side=tk.LEFT)
         tk.Button(self.master, text='Toggle Pagination', command=self.toggle_pagination).pack(side=tk.LEFT)
@@ -49,41 +53,44 @@ class EditableTable(tk.Frame):
         self.filterButton = tk.Button(self.master, text='Filter', command=self.filter_data)
         self.filterButton.pack(side=tk.LEFT)
 
-        # Update table with data for current page
-        self.updateTable()
+        self.exportButton = tk.Button(self.master, text='Export Selected Row', command=self.export_selection)
+        self.exportButton.pack(side=tk.LEFT)
 
-    def updateTable(self):
-        if self.paginationEnabled:
+        # Update table with data for current page
+        self.update_table()
+
+    def update_table(self):
+        if self.pagination_enabled:
             # Calculate start and end rows for current page
-            start_row = self.currentPage * self.rowsPerPage
-            end_row = start_row + self.rowsPerPage
+            start = self.current_page * self.rowsPerPage
+            end = start + self.rowsPerPage
 
             # Update table with data for current page
-            self.table.model.df = self.df_filtered.iloc[start_row:end_row]
+            self.table.model.df = self.data_filtered.iloc[start:end]
             self.table.redraw()
         else:
             # Update table with all data
-            self.table.model.df = self.df_filtered
+            self.table.model.df = self.data_filtered
             self.table.redraw()
 
     def prev_page(self):
         # Go to previous page if not on first page
-        if self.currentPage > 0:
-            self.currentPage -= 1
-            self.updateTable()
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.update_table()
 
     def next_page(self):
         # Go to next page if not on last page
-        if (self.currentPage + 1) * self.rowsPerPage < len(self.df):
-            self.currentPage += 1
-            self.updateTable()
+        if (self.current_page + 1) * self.rowsPerPage < len(self.df):
+            self.current_page += 1
+            self.update_table()
 
     def toggle_pagination(self):
         # Toggle pagination on/off and update table
-        self.paginationEnabled = not self.paginationEnabled
-        if not self.paginationEnabled:
+        self.pagination_enabled = not self.pagination_enabled
+        if not self.pagination_enabled:
             # Reset current page when disabling pagination
-            self.currentPage = 0
+            self.current_page = 0
 
             # Disable prev/next buttons when pagination is disabled
             self.prevButton.config(state=tk.DISABLED)
@@ -94,7 +101,7 @@ class EditableTable(tk.Frame):
             self.nextButton.config(state=tk.NORMAL)
 
         # Update table with data for current page or all data if pagination is disabled
-        self.updateTable()
+        self.update_table()
 
     def filter_data(self):
         # Get selected column and entered value
@@ -103,17 +110,40 @@ class EditableTable(tk.Frame):
 
         if selected_column and filter_value:
             # Filter DataFrame based on selected column and entered value
-            self.df_filtered = self.df[self.df[selected_column].astype(str).str.contains(filter_value)]
+            self.data_filtered = self.df[self.df[selected_column].astype(str).str.contains(filter_value)]
 
             # Reset current page to 0
-            self.currentPage = 0
+            self.current_page = 0
 
             # Update table with filtered data
-            self.updateTable()
+            self.update_table()
         else:
             # If no column/value is selected or entered, show the original data
-            self.df_filtered = self.df
-            self.updateTable()
+            self.data_filtered = self.df
+            self.update_table()
+
+    def export_selection(self):
+        # Get selected row indices
+        selected_row_indices = self.table.multiplerowlist
+
+        if selected_row_indices:
+            # Get selected rows from filtered DataFrame
+            selected_rows = self.data_filtered.iloc[selected_row_indices]
+
+            # Open file dialog to select file to save exported rows
+            file_name = filedialog.asksaveasfilename(
+                defaultextension=".csv", initialfile="export.csv", initialdir=os.path.dirname(file), filetypes=[("CSV files", "*.csv")]
+            )
+
+            if file_name:
+                # Export selected rows to the specified CSV file
+                selected_rows.to_csv(file_name, index=False)
+
+                print(f"Selected rows exported to '{file_name}'")
+            else:
+                print("No file selected")
+        else:
+            print("No rows selected")
 
 
 app = EditableTable(tk.Tk())
