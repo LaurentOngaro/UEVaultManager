4b2d49528e79aa297f195f039f870dbac41d3d81
LaurentOngaro <laurent@gameamea.com>
2023-04-25 19:25:44 +0200

    update todo comments

--- UEVaultManager/tkgui/tkgui.py
+++ UEVaultManager/tkgui/tkgui.py
@@ -7,12 +7,12 @@ Bugs to fix:
 - the url column is nan
 
 To Do:
-- add features and buttons to refresh csv file by calling UEVaultManager cli
+- add features and buttons to refresh csv file by calling UEVaultManager cli (WIP)
+- integrate the code into the UEVaultManager code base (WIP)
 - add more info about the current row (at least comment, review...) in the preview frame
 - edit users fields (comment, alternative...) in the main windows (in the preview frame ?)
 - save and load for tcsv files
 - save and load for json files
-- migrate the code into the UEVaultManager code base
 - document the new features
 - update the PyPi package
 
