a8a79edd9ed775f9226dbb053ef8485fc2ca5699
LaurentOngaro <laurent@gameamea.com>
2023-05-20 08:31:00 +0200

    minor change

--- testing/Done/00a_tk_EditableTable_base.py
+++ testing/Done/00a_tk_EditableTable_base.py
@@ -15,26 +15,15 @@ class EditableTable(tk.Frame):
         self.currentPage = 0
         self.rowsPerPage = 35
         self.paginationEnabled = True
-
-        # Load data from CSV file
         self.df = pd.read_csv(file)
-
-        # Initialize filtered DataFrame
         self.df_filtered = self.df
-
-        # Create frame for table
         f = tk.Frame(self.master)
         f.pack(fill=tk.BOTH, expand=1)
-
-        # Create table to display data
         self.table = Table(f, dataframe=self.df.iloc[0:0], showtoolbar=True, showstatusbar=True)
         self.table.show()
-
-        # Update table with data for current page
         self.update_table()
 
     def update_table(self):
-        # Update table with all data
         self.table.model.df = self.df_filtered
         self.table.redraw()
 
