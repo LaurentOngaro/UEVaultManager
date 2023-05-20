7f741232f43bc8dc91fdf40e084fec8b8e4ecc46
LaurentOngaro <laurent@gameamea.com>
2023-05-20 11:29:36 +0200

    add docstrings

--- UEVaultManager/models/exceptions.py
+++ UEVaultManager/models/exceptions.py
@@ -1,4 +1,12 @@
 # coding: utf-8
+"""
+Implementation for:
+- InvalidCredentialsError: Exception raised when the credentials are invalid
+"""
+
 
 class InvalidCredentialsError(Exception):
+    """
+    Exception raised when the credentials are invalid
+    """
     pass
