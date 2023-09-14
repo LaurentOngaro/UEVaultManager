d599203092be271f36e95a4cc4b1fa359ffb4000
LaurentOngaro <laurent@gameamea.com>
2023-09-14 07:23:19 +0200

    IMP: add copy_folder and compare_folders functions

--- UEVaultManager/lfs/utils.py
+++ UEVaultManager/lfs/utils.py
@@ -1,7 +1,13 @@
 # coding: utf-8
-
+"""
+utilities functions for LFS
+"""
+import filecmp
 import logging
+import os
+import shutil
 from pathlib import Path
+
 
 logger = logging.getLogger('LFS Utils')
 
@@ -31,3 +37,37 @@ def path_join(*paths):
     :return: The joined paths.
     """
     return Path(*paths).resolve().as_posix()
+
+
+def copy_folder(src_folder: str, dest_folder: str, check_copy=True) -> bool:
+    """
+    Copy files from src_folder to dest_folder
+    :param src_folder: source directory
+    :param dest_folder: destination directory
+    :param check_copy: check if copy was successful
+    :return: True if successful, False otherwise
+    """
+    try:
+        if not os.makedirs(dest_folder, exist_ok=True):
+            return False
+        for item in os.listdir(src_folder):
+            src = path_join(src_folder, item)
+            dest = path_join(dest_folder, item)
+            if os.path.isdir(src):
+                shutil.copytree(src, dest, False, None)
+            else:
+                shutil.copy2(src, dest)
+    except (Exception, ):
+        return False
+    return True if not check_copy or not compare_folders(src_folder, dest_folder) else False
+
+
+def compare_folders(folder1: str, folder2: str) -> list:
+    """
+    Compare two directories and return a list of files that are different
+    :param folder1: First directory
+    :param folder2: Second directory
+    :return: List of files that are different
+    """
+    comparison = filecmp.dircmp(folder1, folder2)
+    return comparison.diff_files
