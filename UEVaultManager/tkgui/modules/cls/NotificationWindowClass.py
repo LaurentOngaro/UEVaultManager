90f7d027244ba6f8e284d936660203cf1dee41c6
LaurentOngaro <laurent@gameamea.com>
2023-11-23 17:40:39 +0100

    minor fix and change

--- UEVaultManager/tkgui/modules/cls/NotificationWindowClass.py
+++ UEVaultManager/tkgui/modules/cls/NotificationWindowClass.py
@@ -8,7 +8,6 @@ from tkinter import font
 
 import ttkbootstrap as ttk
 from ttkbootstrap import utility
-from ttkbootstrap.constants import *
 
 import UEVaultManager.tkgui.modules.functions as gui_f  # using the shortest variable name for globals for convenience
 import UEVaultManager.tkgui.modules.functions_no_deps as gui_fn  # using the shortest variable name for globals for convenience
@@ -33,17 +32,13 @@ class NotificationWindow(tk.Toplevel):
     You can click the notification to close it.
     :param message:
         The notification message.
-    :param window_title:
+    :param title:
         The notification title.
-    :param width: width.
-    :param height: height.
-    :param icon:
+    :param display_icon:
         A unicode character to display on the top-left hand corner of the notification. The default symbol is OS specific. Pass an empty string to remove the symbol.
     :param screen_index: screen index.
     :param duration:
         The number of milliseconds to show the notification. If 0 (default), then you must click the notification to close it.
-    :param bootstyle:
-        Style keywords used to update the label style. One of the accepted color keywords.
     :param alert:
         Indicates whether to ring the display bell when the notification is shown.
     :param iconfont (Union[str, Font]):
@@ -53,45 +48,40 @@ class NotificationWindow(tk.Toplevel):
 
     def __init__(
         self,
-        container: tk.Tk,
         message: str,
-        window_title: str = 'Notification',
+        title: str = 'Notification',
+        sub_title: str = '',
         width: int = 400,
         height: int = 100,
-        icon=None,
+        display_icon: str = None,
         screen_index: int = -1,
         duration: int = 0,
-        bootstyle: str = LIGHT,
         alert: bool = False,
-        autosize: bool = True,
         iconfont=None,
     ):
         if gui_g.WindowsRef.notification:
             gui_g.WindowsRef.notification.destroy()
         super().__init__()
-        self.title(window_title)
+        self.title(title)
         try:
             # an error can occur here AFTER a tool window has been opened and closed (ex: db "import/export")
             self.style = gui_fn.set_custom_style(gui_g.s.theme_name, gui_g.s.theme_font)
+            # get the root window
+            root = gui_g.WindowsRef.uevm_gui or self
+            self.screen_index: int = screen_index if screen_index >= 0 else int(root.winfo_screen()[1])
+            self.geometry(gui_fn.center_window_on_screen(self.screen_index, width, height))
         except Exception as error:
             gui_f.log_warning(f'Error in DisplayContentWindow: {error!r}')
-        gui_fn.set_icon_and_minmax(self, icon)
-        self.container = container
-        self.message = message
-        self.duration = duration
-        self.bootstyle = bootstyle
-        self.icon = icon
-        self.width = width
-        self.height = height
-        # get the root window
-        root = gui_g.WindowsRef.uevm_gui or self
-        self.screen_index = screen_index if screen_index >= 0 else int(root.winfo_screen()[1])
-        self.alert = alert
-        self.autosize = autosize
+        gui_fn.set_icon_and_minmax(self)
+        self.sub_title: str = sub_title or gui_g.s.app_title
+        self.display_icon: str = display_icon
+        self.message: str = message
+        self.duration: int = duration
+        self.alert: bool = alert
         self.iconfont = iconfont
         self.frm_control = None
-        self.iconfont = None
-        self.titlefont = None
+        self.iconfont: font = None
+        self.titlefont: font = None
         self.is_visible: bool = True
         gui_g.WindowsRef.notification = self
         self.bind("<ButtonPress>", self.hide)
@@ -104,36 +94,26 @@ class NotificationWindow(tk.Toplevel):
 
         def __init__(self, container):
             super().__init__(container)
-            self.container = container
 
             # noinspection PyArgumentList
-            ttk_item = ttk.Label(self, text=container.icon, font=container.iconfont, bootstyle=f"{container.bootstyle}-inverse", anchor=NW, )
-            ttk_item.grid(row=0, column=0, rowspan=2, sticky=NSEW, padx=(5, 0))
+            ttk_item = ttk.Label(self, text=container.display_icon, font=container.iconfont, anchor=tk.NW, )
+            ttk_item.grid(row=0, column=0, rowspan=2, sticky=tk.NSEW, padx=(5, 0))
+            if container.sub_title:
+                # noinspection PyArgumentList
+                ttk_item = ttk.Label(self, text=container.sub_title, font=container.titlefont, anchor=tk.NW, )
+                ttk_item.grid(row=0, column=1, sticky=tk.NSEW, padx=10, pady=(5, 0))
             # noinspection PyArgumentList
-            ttk_item = ttk.Label(self, text=container.title, font=container.titlefont, bootstyle=f"{container.bootstyle}-inverse", anchor=NW, )
-            ttk_item.grid(row=0, column=1, sticky=NSEW, padx=10, pady=(5, 0))
-            # noinspection PyArgumentList
-            ttk_item = ttk.Label(
-                self, text=container.message, wraplength=utility.scale_size(self, 300), bootstyle=f"{container.bootstyle}-inverse", anchor=NW,
-            )
-            ttk_item.grid(row=1, column=1, sticky=NSEW, padx=10, pady=(0, 5))
+            ttk_item = ttk.Label(self, text=container.message, wraplength=utility.scale_size(self, 300), anchor=tk.NW, )
+            ttk_item.grid(row=1, column=1, sticky=tk.NSEW, padx=10, pady=(0, 5))
 
             self.bind("<ButtonPress>", container.hide)
-
-            # alert notification
-            if container.alert:
-                container.bell()
-
-            # specified duration to close
-            if container.duration:
-                container.after(container.duration, container.hide)
 
     def _init(self) -> None:
         """
         Set up the notification window.
         """
         winsys = self.tk.call('tk', 'windowingsystem')
-        self.configure(relief=RAISED)
+        self.configure(relief=tk.RAISED)
 
         # heading font
         _font = font.nametofont('TkDefaultFont')
@@ -142,20 +122,27 @@ class NotificationWindow(tk.Toplevel):
         self.iconfont = font.Font(size=30, weight='bold')
         if winsys == 'win32':
             self.iconfont['family'] = 'Segoe UI Symbol'
-            self.icon = DEFAULT_ICON_WIN32 if self.icon is None else self.icon
+            self.display_icon = DEFAULT_ICON_WIN32 if self.display_icon is None else self.display_icon
         elif winsys == 'x11':
             self.iconfont['family'] = 'FreeSerif'
-            self.icon = DEFAULT_ICON if self.icon is None else self.icon
+            self.display_icon = DEFAULT_ICON if self.display_icon is None else self.display_icon
         else:
             self.iconfont['family'] = 'Apple Symbols'
-            self.icon = DEFAULT_ICON if self.icon is None else self.icon
+            self.display_icon = DEFAULT_ICON if self.display_icon is None else self.display_icon
 
         self.frm_control = self.ControlFrame(self)
         self.frm_control.pack(fill=ttk.BOTH, expand=ttk.YES)
 
+        # alert notification
+        if self.alert:
+            self.bell()
+
+        # specified duration to close
+        if self.duration:
+            self.after(self.duration, self.hide)
+
         # actualize geometry
         self.update_idletasks()
-        self.container.update_idletasks()  # update the container to get the correct size
 
     def _set_geometry(self) -> None:
         """
@@ -197,9 +184,8 @@ if __name__ == "__main__":
     main.title('FAKE MAIN Window')
     main.geometry('200x100')
     window = NotificationWindow(
-        container=main,
         message=f'This is a notification message.\nYou can either make it appear for a specified period of time, or click to close.',
-        window_title=st.title,
+        title=st.title,
         screen_index=st.screen_index,
         duration=st.duration,
     )
