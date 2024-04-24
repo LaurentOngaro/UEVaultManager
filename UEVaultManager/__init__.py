864ce20d836bccf4ed5c4f7d58de451e183ec0c6
LaurentOngaro <laurent@ongaro.fr>
2024-04-24 17:27:25 +0200

    bump to 1.12.0

--- UEVaultManager/__init__.py
+++ UEVaultManager/__init__.py
@@ -7,7 +7,7 @@ import datetime
 year = datetime.date.today().year
 
 __name__ = 'UEVaultManager'
-__version__ = '1.11.0'
+__version__ = '1.12.0'
 # 0 Pegasus Seiya
 # 1 Dragon Shiryu
 # 2 Cygnus Hyoga
@@ -20,8 +20,8 @@ __version__ = '1.11.0'
 # 8 Sagittarius Aiolos
 # 9 Capricorn Shura
 # 10 Aquarius Camus
-__codename__ = 'Seiya'
 # 11 Pisces Aphrodite
+__codename__ = 'Hyoga'
 __author__ = 'Laurent Ongaro'
 __author_email__ = 'laurent@gameamea.com'
 __description__ = 'Free and open-source replacement for the Epic Games Launcher application, mainly to manage the assets for Unreal Engine'
