95f2311d03ee871199974d0280a99191671b2ee7
LaurentOngaro <laurent@ongaro.fr>
2024-05-12 20:40:47 +0200

    Fix: start scripts

--- scripts/start_gui.bat
+++ scripts/start_gui.bat
@@ -1,8 +1,8 @@
 @echo off
-rem the root foloder of the projet
-set root_folder=D:\Projets_Perso\03d_CodePython\UEVaultManager
+set root_drive=D:
+set root_folder=%root_drive%\Projets_Perso\03d_CodePython\UEVaultManager
 rem we use the venv version of python to start, as it the venv will be loaded as in development
-set python_exe=.\UEVaultManager\.venv\scripts\python.exe
+set python_exe=.\.venv\scripts\python.exe
 rem we load the main file as a MODULE (i.e. without the .py extension) , as it, the path for imports will be OK
 rem see: https://stackoverflow.com/questions/72852/how-can-i-do-relative-imports-in-python#comment5699857_73149
 set module_path=UEVaultManager.tkgui.main
